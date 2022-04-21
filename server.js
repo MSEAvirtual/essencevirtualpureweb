e5a232c6514f8c4d062b101d9a06f6b012401c9a
JFmagicoder <guardian.sanddy@gmail.com>
2022-04-21 16:25:13 +0100

    make it better

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dd = require('dotenv');
+const app = express();
+const path = require('path');
+const DIR= 'build';
+var compression = require('compression')
+app.use(compression());
+dd.config();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.static(DIR));
+
+console.log(process.env.PORT, PORT);
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, DIR, '/index.html'));
+});
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
