d5cd3d0ae88551031ef6f5b692a33ce60939187b
JFmagicoder <guardian.sanddy@gmail.com>
2022-06-06 17:15:29 +0100

    Fix: Stretching on desktop

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.static(DIR));
 
-console.log(process.env.PORT, PORT);
-
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname, DIR, '/index.html'));
