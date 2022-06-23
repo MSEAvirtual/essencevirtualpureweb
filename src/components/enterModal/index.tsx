09553f9e2c7515860268163f68e54e21bd47e62e
JFmagicoder <guardian.sanddy@gmail.com>
2022-06-23 19:25:33 +0100

    Fix: Updated Essence

--- src/components/enterModal/index.tsx
+++ src/components/enterModal/index.tsx
@@ -1,39 +1,61 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useState } from "react";
 import { Button } from "semantic-ui-react";
+import { isMobileTablet } from "../../utils";
 import "./index.css";
+// import videoFile from "../../introVideo.mp4";
 
 const EnteModalView = ({ storeData, data, closeModal, setClose }: any) => {
   const [showMap, setShowMap] = useState(false);
+  const isMobile = isMobileTablet();
 
-  const RenderIntro = () => {
+  const handleVideoEnded = () => setShowMap(true);
+
+  const RenderVideoDesktop = () => {
     return (
-      <div className="intro-div">
-        <img src="/loadinglogo.png" className="introLogo" alt="intro-logo" />
-        <h1 className="title-first">WELCOME TO ESSENCE FEST OF CULTURE</h1>
-        <pattern className="title-second">VIRTUAL MARKETPLACE 2022!</pattern>
-        <p className="text-first text-inline">Use these <img src="/button.png" alt="td-op" /> to navigate through the marketplace.</p>
-        <div className="tm text-inline">
-          <img src="/shop.png" alt="td-op" />SHOP <img className="ml-2" src="/question.png" alt="td-op" /> LEARN <img className="ml-2" src="/video.png" alt="td-op" /> WATCH
-        </div>
-        <p className="text-first strong">Over 28 Black Owned Businesses!</p>
-        <div className="intro-button">
-          <Button className="store-btn" onClick={() => setShowMap(true)}>View Map <br /> Start Your Visit</Button>
-        </div>
-      </div>
+      <>
+        <video className="intro-video" autoPlay onEnded={handleVideoEnded}>
+          <source src="/introVideo.mp4" type="video/mp4" />
+        </video>
+      </>
     )
   }
 
+  const RenderIntro = () => {
+    // return (
+    //   <div className="intro-div">
+    //     <img src="/loadinglogo.png" className="introLogo" alt="intro-logo" />
+    //     <h1 className="title-first">WELCOME TO THE VIRTUAL MARKETPLACE 2022!</h1>
+    //     <p className="text-first text-inline">Use these <img src="/button.png" alt="td-op" /> to navigate through the marketplace.</p>
+    //     <p className="text-first text-inline">Press <img src="/menu.png" alt="td-op" /> to access the menu.</p>
+    //     <div className="tm text-inline">
+    //       <img src="/shop.png" alt="td-op" />SHOP <img className="ml-2" src="/question.png" alt="td-op" /> LEARN <img className="ml-2" src="/video.png" alt="td-op" /> WATCH
+    //     </div>
+    //     <p className="text-first strong">Over 28 Black Owned Businesses!</p>
+    //     <div className="intro-button">
+    //       <Button className="store-btn" onClick={() => setShowMap(true)}>View Map <br /> Start Your Visit</Button>
+    //     </div>
+    //   </div>
+    // )
+    return (<div className="intro-image" style={{ backgroundImage: `url("/intro-mobile.png")`}}>
+      <button className="intro-button" onClick={() => setShowMap(true)} />
+    </div>)
+  }
+
   const RenderMap = () => {
-    return (<img className="map-image" src="/map.jpg" alt="map-im" />)
+    // return (<img className="map-image" src={isMobile ? "/map-mobile.png" : "/map-desktop.png"} alt="map-im" />)
+    return (<div className="map-image" style={{ background: `url(${isMobile ? "map-mobile.png" : "map-desktop.png"})` }}>
+      <button className="map-button" onClick={closeModal} />
+    </div>)
   }
 
   return (
-    <div className={"store-body-view"}>
-      <div className={`flex-row-container ${showMap ? "p-0" : ""}`}>
-        {!showMap ? RenderIntro() : RenderMap()}
-        <span className="store-close-btn" onClick={closeModal}>X</span>
+    <div className={"store-body-m-view"}>
+      <div className={`flex-row-m-container ${showMap ? "p-0" : ""}`}>
+        {!showMap ? !isMobile ? RenderVideoDesktop() : RenderIntro() : RenderMap()}
+        {/* <span className="store-close-btn" onClick={closeModal}>X</span> */}
       </div>
     </div>
   );
--- src/components/modals/ARmodalPopUp.tsx
+++ src/components/modals/ARmodalPopUp.tsx
@@ -1,15 +1,23 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 
 import React from "react"
+import "./ar.css"
 
-const ARURL = process.env.REACT_APP_AR_URL;
+const ARURL = process.env.REACT_APP_AR_URL || "https://theimmersivepath.8thwall.app/essence";
 const width = "100%", height = "100%";
 
-const ARModalPopUp = () => {
+const ARModalPopUp = ({ closeModal }:any) => {
     return (
-        <div style={{
+        <>
+        <div id="rotate-view">
+            <p className="rotate-mobile">Please Rotate Sceen to Potriat Mode to Use AR</p>
+        </div>
+        <div id="ar-view" style={{
             width, height
         }} dangerouslySetInnerHTML={{ __html: `<iframe src=${ARURL} width="${width}" height="${height}"  allow="camera *;microphone *" />`}} />
-        // <>Hello</>
+        <span className="store-close-btn" onClick={closeModal}>X</span>
+        </>
     )
 }
 
