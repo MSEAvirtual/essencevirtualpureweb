5ca81c5f846b3113ede0c9fe0cf608f1b14c307e
Sidney Douw <s.douw@gmx.de>
2022-06-24 20:14:57 +0200

    ar modal layout fix

--- src/components/modals/ARmodalPopUp.tsx
+++ src/components/modals/ARmodalPopUp.tsx
@@ -1,24 +1,63 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 
-import React from "react"
-import "./ar.css"
+import React, { useEffect, useRef, useState } from "react";
+import { isMobileTablet } from "../../utils";
+import "./ar.css";
 
 const ARURL = process.env.REACT_APP_AR_URL || "https://essencephotobooth.live/";
-const width = "100%", height = "100%";
 
-const ARModalPopUp = ({ closeModal }:any) => {
-    return (
-        <>
-        <div id="rotate-view">
-            <p className="rotate-mobile">Please Rotate Sceen to Potriat Mode to Use AR</p>
-        </div>
-        <div id="ar-view" style={{
-            width, height
-        }} dangerouslySetInnerHTML={{ __html: `<iframe src=${ARURL} width="${width}" height="${height}"  allow="camera *;microphone *" />`}} />
-        <span className="store-close-btn" onClick={closeModal}>X</span>
-        </>
-    )
-}
+const ARModalPopUp = ({ closeModal }: any) => {
+  const arView = useRef<HTMLDivElement>(null);
+  const [dimensions, setDimensions] = useState({
+    width: "100%",
+    height: "100%",
+  });
+
+  useEffect(() => {
+    setDimensions(calculateRatio());
+  }, []);
+
+  const calculateRatio = () => {
+    if (arView && arView.current) {
+      const windowRatio = window.innerWidth / window.innerHeight;
+      const modalRatio =
+        arView.current!.clientWidth / arView.current!.clientHeight;
+
+      if (isMobileTablet()) {
+        return { width: "100%", height: "100%" };
+      } else {
+        if (modalRatio < windowRatio) {
+          return { width: "100%", height: arView.current.clientWidth + "px" };
+        } else {
+          return { width: arView.current.clientHeight + "px", height: "100%" };
+        }
+      }
+    }
+
+    return { width: "100%", height: "100%" };
+  };
+
+  return (
+    <>
+      <div id="rotate-view">
+        <p className="rotate-mobile">
+          Please Rotate Sceen to Potriat Mode to Use AR
+        </p>
+      </div>
+      <div
+        ref={arView}
+        id="ar-view"
+        style={{ width: "100%", height: "100%" }}
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src=${ARURL} width="${dimensions.width}" height="${dimensions.height}" allow="camera *;microphone *" />`,
+        }}
+      />
+      <span className="store-close-btn" onClick={closeModal}>
+        X
+      </span>
+    </>
+  );
+};
 
 export default ARModalPopUp;
