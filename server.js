0a191937d93e88f2232a2d7e289b68f83f557cc8
JFmagicoder <guardian.sanddy@gmail.com>
2022-06-27 16:00:51 +0100

    Fix: Enforce Redirect

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-const express = require('express');
-const dd = require('dotenv');
+const express = require("express");
+const dd = require("dotenv");
 const app = express();
-const path = require('path');
-const DIR= 'build';
-var compression = require('compression')
-app.use(compression());
+const path = require("path");
+const compression = require("compression");
 dd.config();
-const PORT = process.env.PORT || 5001;
 
+const PORT = process.env.PORT || 5001;
+const DIR = "build";
+app.use(compression());
 app.use(express.static(DIR));
+app.use(function (environments, status) {
+  environments = environments || ["production"];
+  status = status || 302;
+  return function (req, res, next) {
+    if (environments.indexOf(process.env.NODE_ENV) >= 0) {
+      if (req.headers["x-forwarded-proto"] != "https") {
+        res.redirect(status, "https://" + req.hostname + req.originalUrl);
+      } else {
+        next();
+      }
+    } else {
+      next();
+    }
+  };
+});
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, DIR, '/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, DIR, "/index.html"));
 });
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
